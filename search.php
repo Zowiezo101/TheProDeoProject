ba89378ebc5bf69c3754588cba2ee155b27f97ec
zgeurts <devnull@localhost>
2017-07-09 14:44:46 +0200

    Added two new files. One for search results and one for settings.

---
+++ search.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<?php require "layout/header.php"; ?>
+	
+	<div>
+		<h1>Contents</h1>
+	</div>
+	
+	<?php require "layout/footer.php" ?>
+</html>
---
+++ settings.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<?php require "layout/header.php"; ?>
+	
+	<!-- Important, do something with sessions here!! -->
+	<div class="clearfix">
+		<div class="contents_left" id="settings">
+			<h1>Left</h1>
+		</div>
+		
+		<div class="contents_right" id="settings">
+			<h1>Right</h1>
+		</div>
+	</div>
+	
+	<?php require "layout/footer.php" ?>
+</html>
