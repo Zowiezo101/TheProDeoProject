f940d09fe4a2894c508cca9051a0b2b60f4e6f17
zgeurts <devnull@localhost>
2017-07-14 19:22:14 +0200

    Working on Peoples part, can now display the ID number in the info section by clicking a button in the bar section.

--- database/database.php
+++ database/database.php
@@ -1,4 +1,5 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -8,34 +9,34 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
 
 function GetListOfPeoples() {
 	global $conn;
 	
-	$sql = "SELECT ID,Name FROM peoples";
+	$sql = "SELECT ID,Name FROM peoples LIMIT 100";
 	$result = $conn->query($sql);
 	
 	if (!$result) {
-		echo "<h1>Could not get results..</h1>\n";
+		echo("<h1>Could not get results..</h1>\n");
 	}
 	else {
-		echo "<ul>";
 		while ($name = $result->fetch_array()) {
-			echo "<li><a href='?id=".$name['ID']."'>".$name['Name']."</a></li>";
+			echo "<form method='post' action='peoples.php'>";
+			echo "<input type='hidden' name='id' value='".$name['ID']."'</button>";
+			echo "<input type='submit' name='submit' value='".$name['Name']."'</button>";
+			echo "</form>\n";
 		}
-		echo "</ul>\n";
 	}
 }
 
-function GetPeopleInfo() {
+function GetPeopleInfo($ID) {
 	global $conn;
 	
-	$ID = _GET['id'];
-	
-	$sql = "SELECT * FROM peoples WHERE ID==".$ID;
+	$sql = "SELECT * FROM peoples WHERE ID=".$ID;
 	$result = $conn->query($sql);
+	$people = NULL;
 	
 	if (!$result) {
 		echo "<h1>Could not get results..</h1>\n";
@@ -47,20 +48,3 @@ function GetPeopleInfo() {
 	return $people;
 }
 ?>
-
-<script>
-window.onload = function updatePagePeople() {
-	var contentEl = document.getElementById("people_info");
-	var ID = -1;
-	
-	<?php echo "ID = ".$_GET['id'].";"; ?>
-	
-	// <?php 
-	// $people = GetPeopleInfo(); 
-	// echo "var ID = ".$people['ID'].";"
-	// // echo "contentEl.innerHTML = ".$people["ID"].";"
-	// ?>
-	
-	alert(ID);
-}
-</script>
--- peoples.php
+++ peoples.php
@@ -13,5 +13,31 @@
 		</div>
 	</div>
 	
-	<?php require "layout/footer.php" ?>
+	<?php require "layout/footer.php"; ?>
 </html>
+
+<!-- TODO: Make sure to add some buttons to reach the other xxx names as well..
+	It is now limited to 100 names only.. -->
+<?php
+if (isset($_POST['submit'])) {
+?>
+	<script>
+		var contentEl = document.getElementById("people_info");
+		
+		<?php 
+			$information = GetPeopleInfo($_POST['id']); 
+		?>
+		
+		// var ID = <?php echo "'".$_POST['id']."'"; ?>;
+		var ID = <?php 
+			echo "'".$information['ID']."'"; 
+		?>;
+		
+		// Create a Table
+		// Left is key names
+		// right is value names
+		contentEl.innerHTML = ID;
+	</script>
+<?php
+}
+?>
