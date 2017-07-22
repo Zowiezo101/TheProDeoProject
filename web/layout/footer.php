ea7f3a76b5110c0d57df6ec98c47f978ceff14da
zgeurts <devnull@localhost>
2017-07-22 21:23:43 +0200

    Updated footer contents

--- web/layout/footer.php
+++ web/layout/footer.php
@@ -1,4 +1,16 @@
 	<div class="footer">
-		<?php echo $Footer["PP_Name"]; ?>
+		<?php echo $Footer["PP_Name"]; ?>&copy <?php echo date("Y"); ?>
+		<br>
+		<?php 
+			echo $Footer["PP_Version"].": v1.0. ";
+			
+			$uri_parts = explode('?', basename($_SERVER['REQUEST_URI'], 2));
+			$current_page = $uri_parts[0];
+			
+			$date_page = filemtime($current_page);
+			
+			date_default_timezone_set('Europe/Amsterdam');
+			echo $Footer["PP_date"]." ".date("d-m-Y H:i", $date_page); 
+		?>
 	</div>
 </body>
