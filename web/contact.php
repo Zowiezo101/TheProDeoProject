9ec66e07be0b16497623aa6ed0be5bf40ae10090
zgeurts <devnull@localhost>
2017-07-23 15:27:05 +0200

    Added a contact page

--- web/contact.php
+++ web/contact.php
@@ -2,8 +2,12 @@
 <html>
 	<?php require "layout/header.php"; ?>
 	
-	<div>
-		<h1><?php echo $Content["tbd"]; ?></h1>
+	<div id="contact">
+		<h1><?php echo $Contact["welcome"]; ?></h1>
+		<p><?php echo $Contact["aboutus"]; ?></p>
+		<p><?php echo $Contact["info"]; ?></p>
+		<p><?php echo $Contact["contact"]; ?></p>
+		<p><?php echo $Contact["other"]; ?></p>
 	</div>
 	
 	<?php require "layout/footer.php" ?>
