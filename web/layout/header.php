15557825b8be44b2d3681d78b5dca2c68cf102a0
zgeurts <devnull@localhost>
2017-09-03 20:21:00 +0200

    When changing language, stay on the same page

--- web/layout/header.php
+++ web/layout/header.php
@@ -66,10 +66,55 @@
 <script>
 	function Language(newLang) {		
 		if (newLang == "nl") {
-			window.location.href = "home.php";
+			window.location.href = removeURLParameter(window.location.href, "lang");
 		} else {
-			window.location.href = "home.php?lang=" + newLang;
+			window.location.href = updateURLParameter(window.location.href, "lang", newLang);
 		}
+	}
+	
+	/**
+	* http://stackoverflow.com/a/10997390/11236
+	*/
+	function updateURLParameter(url, param, paramVal){
+		var newAdditionalURL = "";
+		var tempArray = url.split("?");
+		var baseURL = tempArray[0];
+		var additionalURL = tempArray[1];
+		var temp = "";
+		
+		if (additionalURL) {
+			tempArray = additionalURL.split("&");
+			
+			for (var i=0; i<tempArray.length; i++){
+				if(tempArray[i].split('=')[0] != param){
+					newAdditionalURL += temp + tempArray[i];
+					temp = "&";
+				}
+			}
+		}
+
+		var rows_txt = temp + "" + param + "=" + paramVal;
+		return baseURL + "?" + newAdditionalURL + rows_txt;
+	}
+	
+	function removeURLParameter(url, param){
+		var newAdditionalURL = "";
+		var tempArray = url.split("?");
+		var baseURL = tempArray[0];
+		var additionalURL = tempArray[1];
+		var temp = "?";
+		
+		if (additionalURL) {
+			tempArray = additionalURL.split("&");
+			
+			for (var i=0; i<tempArray.length; i++){
+				if(tempArray[i].split('=')[0] != param){
+					newAdditionalURL += temp + tempArray[i];
+					temp = "&";
+				}
+			}
+		}
+		return baseURL + newAdditionalURL;
 	}
 	
 	function LogIn() {
