1e36e8f416b9eb1430ad50441253d63d3bb15900
zgeurts <devnull@localhost>
2017-09-19 09:35:28 -0400

    Getting prefered language from browsert if available

--- web/tools/baseHelper.php
+++ web/tools/baseHelper.php
@@ -1,8 +1,49 @@
 <?php
 
+// https://stackoverflow.com/questions/3770513/detect-browser-language-in-php	
+function prefered_language(array $available_languages, $http_accept_language) {
+
+	$available_languages = array_flip($available_languages);
+
+	$langs;
+	$langsSet;
+	
+	preg_match_all('~([\w-]+)(?:[^,\d]+([\d.]+))?~', strtolower($http_accept_language), $matches, PREG_SET_ORDER);
+	foreach($matches as $match) {
+
+		list($a, $b) = explode('-', $match[1]) + array('', '');
+		$value = isset($match[2]) ? (float) $match[2] : 1.0;
+
+		if(isset($available_languages[$match[1]])) {
+			$langs[$match[1]] = $value;
+			continue;
+		}
+
+		if(isset($available_languages[$a])) {
+			$langs[$a] = $value - 0.1;
+		}
+
+	}
+	arsort($langs);
+	foreach ($langs as $lang => $value) {
+		$langsSet[] = $lang;
+	}
+
+	return $langsSet;
+}
+
 // Set the language to a default
 if (!isset($_SESSION["lang"])) {
-	$_SESSION["lang"] = "nl";
+	// Languages we support
+	$available_languages = array("en", "nl");
+
+	$langs = prefered_language($available_languages, $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+	if ($langs) {
+		$_SESSION["lang"] = $langs[0];
+	} else {
+		$_SESSION["lang"] = "en";
+	}
+
 } 
 $page_lang = $_SESSION["lang"];
 
