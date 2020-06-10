f1c087db255c3022788d13a5927a49889808622b
Zowiezo101 <devnull@localhost>
2020-06-10 13:31:13 +0200

    Give along query with result

--- web/api/item_read.php
+++ web/api/item_read.php
@@ -4,6 +4,7 @@ require "../../login_data.php";
 class result {
     public $data;
     public $error;
+    public $query;
 };
 
 $result = new result();
@@ -18,16 +19,20 @@ if ($conn->connect_error) {
         // Get the table and the ID that we want to read
         $table = filter_input(INPUT_GET, 'table');
 
-        // No ID given means we want all results of that table
+        
         $value = filter_input(INPUT_GET, 'value') !== null ? filter_input(INPUT_GET, 'value') : "";
         if ($value !== "") {
+            // We just want this specific ID
             $column = filter_input(INPUT_GET, 'column') !== null ? filter_input(INPUT_GET, 'column') : substr($table, 0, -1)."_id";
             $sql = "select * from ".$table." where ".$column." = ".$value;
         } else {
-            $sql = "select * from ".$table;
+            // No ID given means we want all results of that table, or a subset using range
+            $offset = filter_input(INPUT_GET, 'offset') !== null ? " limit ".filter_input(INPUT_GET, 'offset').", 100" : "";
+            $sql = "select * from ".$table.$offset;
         }
 
         // excecute SQL statement
+        $result->query = $sql;
         $results = mysqli_query($conn, $sql);
 
         // die if SQL statement failed
