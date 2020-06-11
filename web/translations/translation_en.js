be888e9d12bfd6649f5a7fd8dba7f78e574596fa
Zowiezo101 <devnull@localhost>
2020-06-11 20:27:10 +0200

    Updated translation files

---
+++ web/translations/translation_en.js
@@ -0,0 +1,285 @@
+var dict_PeoplesParams = {
+	"people_id": "ID number",
+	"name": "Name",
+	"descr": "Description",
+	"meaning_name": "Meaning of name",
+	"father_id": "Father",
+	"father_age": "Age father",
+	"mother_id": "Mother",
+	"mother_age": "Age mother",
+	"age": "Age",
+	"gender": "Gender",
+	"tribe": "Tribe",
+	"profession": "Profession",
+	"nationality": "Nationality",
+	"book_start_vers": "First appearance",
+	"book_end_vers": "Last appearance",
+};
+
+var dict_LocationsParams = {
+	"location_id": "ID number",
+	"name": "Name",
+	"descr": "Description",
+	"meaning_name": "Meaning of name",
+	"type": "Type",
+	"inhabitants": "Population",
+	"coordinates": "Coordinates",
+	"book_start_vers": "First appearance",
+	"book_end_vers": "Last appearance",
+};
+
+var dict_SpecialsParams = {
+	"special_id": "ID number",
+	"name": "Name",
+	"descr": "Description",
+	"meaning_name": "Meaning of name",
+	"type": "Type",
+	"book_start_vers": "First appearance",
+	"book_end_vers": "Last appearance",
+};
+
+var dict_BooksParams = {
+	"book_id": "ID number",
+	"name": "Name",
+	"descr": "Description",
+	"num_chapters": "Number of chapters",
+	"summary": "Summary",
+};
+
+var dict_EventsParams = {
+	"event_id": "ID number",
+	"name": "Name",
+	"descr": "Description",
+	"length": "Length",
+	"date": "Date",
+	"book_start_vers": "Start verse",
+	"book_end_vers": "End verse",
+};
+
+var dict_Home = {
+	"link_blog": "Click to show the rest of this blog",
+	"unlink_blog": "Click to read less",
+	"user_blog": "by",
+};
+
+var dict_Settings = {
+	"welcome": "<h1>Welcome to the login page!</h1><p>Here you can perform the following actions:<p>",
+	"new_blog": "New blog",
+	"delete_blog": "Delete blog",
+	"edit_blog": "Edit blog",
+	"blog_added": "Blog added",
+	"blog_removed": "Blog deleted",
+	"blog_edited": "Blog edited",
+	"title": "Title",
+	"text": "Text",
+	"default": "Choose an option",
+	"login": "Login",
+	"logout": "Logout",
+	"user": "Username",
+	"password": "Password",
+	"incorrect": "Username or password is incorrect",
+};
+
+var dict_NavBar = {
+	"Home": "Home",
+	"Database": "Database",
+	"Peoples": "Peoples",
+	"Locations": "Locations",
+	"Specials": "Specials",
+	"Books": "Books",
+	"Events": "Events",
+	"Search": "Search database",
+	"Timeline": "Timeline",
+	"Timeline_ext": "Timeline",
+	"Worldmap": "Worldmap",
+	"Familytree": "Family tree",
+	"ProDeo": "ProDeo Projects",
+	"Aboutus": "About us",
+	"Contact": "Contact",
+};
+
+var dict_Books = {
+	"default": "<h1>Books</h1>Click on a name in the list for more information.<br><br>Use the buttons with the arrows on the left side to go to the previous/next 100 names (if applicable).<br><br>The buttons with the arrows pointing down, are for choosing how the names are sorted:<br>- <b>Alphabetical</b> (A on top, Z on the bottom)<br>- <b>Reversed alphabetical</b> (Z on top, A on the bottom)<br>- <b>Appearance in the bible</b> (1 on top, 9 on the bottom)<br>- <b>Reversed appearance in the bible</b> (9 on top, 1 on the bottom)<br><br>As soon as you select the sorting method, the button will change into the reversed method of what you selected.",
+};
+
+var dict_Events = {
+	"default": "<h1>Events</h1>Click on a name in the list for more information.<br><br>Use the buttons with the arrows on the left side to go to the previous/next 100 names (if applicable).<br><br>The buttons with the arrows pointing down, are for choosing how the names are sorted:<br>- <b>Alphabetical</b> (A on top, Z on the bottom)<br>- <b>Reversed alphabetical</b> (Z on top, A on the bottom)<br>- <b>Appearance in the bible</b> (1 on top, 9 on the bottom)<br>- <b>Reversed appearance in the bible</b> (9 on top, 1 on the bottom)<br><br>As soon as you select the sorting method, the button will change into the reversed method of what you selected.",
+	"map_event": "List of time lines this event is included in:",
+};
+
+var dict_Peoples = {
+	"default": "<h1>Peoples</h1>Click on a name in the list for more information.<br><br>Use the buttons with the arrows on the left side to go to the previous/next 100 names (if applicable).<br><br>The buttons with the arrows pointing down, are for choosing how the names are sorted:<br>- <b>Alphabetical</b> (A on top, Z on the bottom)<br>- <b>Reversed alphabetical</b> (Z on top, A on the bottom)<br>- <b>Appearance in the bible</b> (1 on top, 9 on the bottom)<br>- <b>Reversed appearance in the bible</b> (9 on top, 1 on the bottom)<br><br>As soon as you select the sorting method, the button will change into the reversed method of what you selected.",
+	"map_people": "List of family trees this person is included in:",
+};
+
+var dict_Locations = {
+	"default": "<h1>Locations</h1>Click on a name in the list for more information.<br><br>Use the buttons with the arrows on the left side to go to the previous/next 100 names (if applicable).<br><br>The buttons with the arrows pointing down, are for choosing how the names are sorted:<br>- <b>Alphabetical</b> (A on top, Z on the bottom)<br>- <b>Reversed alphabetical</b> (Z on top, A on the bottom)<br>- <b>Appearance in the bible</b> (1 on top, 9 on the bottom)<br>- <b>Reversed appearance in the bible</b> (9 on top, 1 on the bottom)<br><br>As soon as you select the sorting method, the button will change into the reversed method of what you selected.",
+};
+
+var dict_Specials = {
+	"default": "<h1>Specials</h1>Click on a name in the list for more information.<br><br>Use the buttons with the arrows on the left side to go to the previous/next 100 names (if applicable).<br><br>The buttons with the arrows pointing down, are for choosing how the names are sorted:<br>- <b>Alphabetical</b> (A on top, Z on the bottom)<br>- <b>Reversed alphabetical</b> (Z on top, A on the bottom)<br>- <b>Appearance in the bible</b> (1 on top, 9 on the bottom)<br>- <b>Reversed appearance in the bible</b> (9 on top, 1 on the bottom)<br><br>As soon as you select the sorting method, the button will change into the reversed method of what you selected.",
+};
+
+var dict_Search = {
+	"Search": "Search..",
+	"Options": "Search options: <br>",
+	"Category": "Select a category",
+	"NoResults": "Can't find any results..",
+	"Results": " results found for ",
+	"Result": " result found for ",
+	"Show": "Show results for ",
+	"All": "All categories",
+	"default_search": "<h1>Search</h1>Use the options on the left to get search results.<br/>Tip: If you don't know the name, but you do know other information, leave the name field empty.<br/>Also, use ; to seperate multiple names (doesn't work for every field).",
+	"bible_book": "Book",
+	"bible_chap": "Chapter",
+	"bible_verse": "Verse",
+	"busy": "Still searching..",
+	"Remove": "Remove all search options and results",
+	"all": "All",
+};
+
+var select_Search_specials = {
+	"object": "Object",
+	"idol": "Idol",
+	"grave": "Grave",
+	"song": "Song",
+	"building": "Building",
+	"creature": "Creature",
+	"world": "World",
+};
+
+var select_Search_locations = {
+	"well": "Well",
+	"river": "River",
+	"mountain": "Mountain",
+	"valley": "Valley",
+	"country": "Country",
+	"district": "District",
+	"county": "County",
+	"city": "City",
+	"object": "Object",
+};
+
+var select_Search_gender = {
+	"male": "Male",
+	"female": "Female",
+};
+
+var select_Search_tribes = {
+	"ruben": "Reuben",
+	"simeon": "Simeon",
+	"levi": "Levi",
+	"juda": "Judah",
+	"dan": "Dan",
+	"naftali": "Naphtali",
+	"gad": "Gad",
+	"aser": "Asher",
+	"issachar": "Issachar",
+	"zebulon": "Zebulun",
+	"jozef": "Joseph",
+	"benjamin": "Benjamin",
+};
+
+var dict_Timeline = {
+	"unknown": "Unknown",
+	"second": "Second",
+	"seconds": "Seconds",
+	"minute": "Minute",
+	"minutes": "Minutes",
+	"hour": "Hour",
+	"hours": "Hours",
+	"day": "Day",
+	"days": "Days",
+	"week": "Week",
+	"weeks": "Weeks",
+	"month": "Month",
+	"months": "Months",
+	"year": "Year",
+	"years": "Years",
+	"decade": "Decade",
+	"decades": "Decades",
+	"century": "Century",
+	"centuries": "Centuries",
+	"millennium": "Millennium",
+	"millennia": "Millennia",
+	"default_tl": "<h1>Timeline</h1>Click on a name in the list for more information<br><br>As soon as you select a timeline and it has loaded, you will be provided with a couple of options for navigating:<br>- <b>Zoom to fit</b>: This option will set the zoomlevel to a value needed to see the entire timeline.<br>- <b>Reset zoom</b>: This resets the zoomlevel and the position of the timeline back to its default values.<br>- <b>Download timeline</b>: Download the timeline as a '.svg' file. This file can be opened using a browser.<br>- <b>Zoom buttons</b>: The plus and the minus button, can be used to zoom in and out.<br>- <b>Panning</b>: By dragging the timeline around.<br>- <b>Scroll to zoom</b>: Doesn't work in all browsers!<br>",
+	"loading_tl": "<h1>Loading Timeline..</h1>",
+	"link_event": "Click to see more information about this event",
+	"zoomfit": "Zoom to fit",
+	"zoomreset": "Reset zoom",
+	"download": "Download timeline",
+	"download_extra": "SVG file. Can be ",
+	"download_extra2": "viewed in web browser",
+};
+
+var dict_Timeline_ext = {
+	"unknown": "Unknown",
+	"second": "Second",
+	"seconds": "Seconds",
+	"minute": "Minute",
+	"minutes": "Minutes",
+	"hour": "Hour",
+	"hours": "Hours",
+	"day": "Day",
+	"days": "Days",
+	"week": "Week",
+	"weeks": "Weeks",
+	"month": "Month",
+	"months": "Months",
+	"year": "Year",
+	"years": "Years",
+	"decade": "Decade",
+	"decades": "Decades",
+	"century": "Century",
+	"centuries": "Centuries",
+	"millennium": "Millennium",
+	"millennia": "Millennia",
+	"default_tl": "<h1>Timeline</h1>Click on a name in the list for more information<br><br>As soon as you select a timeline and it has loaded, you will be provided with a couple of options for navigating:<br>- <b>Zoom to fit</b>: This option will set the zoomlevel to a value needed to see the entire timeline.<br>- <b>Reset zoom</b>: This resets the zoomlevel and the position of the timeline back to its default values.<br>- <b>Download timeline</b>: Download the timeline as a '.svg' file. This file can be opened using a browser.<br>- <b>Zoom buttons</b>: The plus and the minus button, can be used to zoom in and out.<br>- <b>Panning</b>: By dragging the timeline around.<br>- <b>Scroll to zoom</b>: Doesn't work in all browsers!<br>",
+	"loading_tl": "<h1>Loading Timeline..</h1>",
+	"link_event": "Click to see more information about this event",
+	"zoomfit": "Zoom to fit",
+	"zoomreset": "Reset zoom",
+	"download": "Download timeline",
+	"download_extra": "SVG file. Can be ",
+	"download_extra2": "viewed in web browser",
+};
+
+var dict_Familytree = {
+	"default_ft": "<h1>Familytree</h1>Click on a name in the list for more information<br><br>As soon as you select a familytree and it has loaded, you will be provided with a couple of options for navigating:<br>- <b>Zoom to fit</b>: This option will set the zoomlevel to a value needed to see the entire familytree.<br>- <b>Reset zoom</b>: This resets the zoomlevel and the position of the familytree back to its default values.<br>- <b>Download familytree</b>: Download the familytree as a '.svg' file. This file can be opened using a browser.<br>- <b>Zoom buttons</b>: The plus and the minus button, can be used to zoom in and out.<br>- <b>Panning</b>: By dragging the familytree around.<br>- <b>Scroll to zoom</b>: Doesn't work in all browsers!<br>",
+	"loading_ft": "<h1>Loading Family Tree..</h1>",
+	"link_people": "Click to see more information about this person",
+	"zoomfit": "Zoom to fit",
+	"zoomreset": "Reset zoom",
+	"download": "Download familytree",
+	"download_extra": "SVG file. Can be ",
+	"download_extra2": "viewed in web browser",
+};
+
+var dict_Worldmap = {
+	"default_wm": "<h1>Worldmap</h1>Click on a name in the list for more information<br><br>The worldmap is displayed using Google Maps, with markers for all known locations.<br>Just use this map in the same way that you would use Google Maps.",
+	"loading_wm": "<h1>Loading Worldmap</h1>",
+	"link_location": "Click to see more information about this location",
+};
+
+var dict_Contact = {
+	"welcome": "Welcome!",
+	"aboutus": "How nice of you to drop by on the contact page! Let me introduce myself.. I am part of the \"ProDeo Projects\" team. ProDeo means \"For God\", but is also means \"free\". That is also the way I want others to receive my creations: free and for God.</p><p>Right now I'm working alone, but I'm looking for people who are willing to help me to make this website a handy tool for the Bible. I also have some other plans to introduce the Bible to others in a interesting way, to make it easier to become familiar with its valuable contents.",
+	"info": "This website can be used as a handy tool for the Bible. There is a database available with people, locations, biblebooks, events and some other stuff from the Bible. There is also a timeline with events, a worldmap with locations and a family tree with people from the database. As much information as possible is collected (like parents, place of residence, founders of cities, ages etc.). Please be aware that the database currently is based on the dutch bible translation abbriviated with \"NBV\"!",
+	"contact": "Do you have any questions, tips, remarks (like typos..) about the website or its contents, or do you want to help with something?<br>Then send an email to ProDeoProductions2u@gmail.com, or fill in the form down below to send it anonymously",
+	"contact_form": "Contact form",
+	"contact_subject": "Subject",
+	"contact_text": "Message",
+	"contact_submit": "Submit form",
+	"contact_failed": "Sending message failed.. Here is the error message:",
+	"contact_succes": "The message is sent!",
+	"other": "For God, to you!<br>ProDeo Projects&copy",
+};
+
+var dict_Footer = {
+	"PP_name": "ProDeo Projects",
+	"PP_version": "Current version",
+	"PP_date": "Page was last modified on",
+	"DB_website": "https://bijbel.eo.nl/bijbel/",
+	"slogan": "Bible database - For God, to you!",
+};
+
