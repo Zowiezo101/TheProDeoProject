6badb19a124ca979613778d5bf67e7b20708e0a0
Zowiezo101 <devnull@localhost>
2020-11-30 13:09:15 +0100

    Not passed on to search.js

--- web/api/items_search.php
+++ web/api/items_search.php
@@ -5,8 +5,6 @@ class result {
     public $data;
     public $error;
     public $query;
-    public $table;
-    public $num_res;
 };
 
 $result = new result();
@@ -20,7 +18,6 @@ if ($conn->connect_error) {
     if (filter_input(INPUT_GET, 'table') !== null) {
         // Get the table and the ID that we want to read
         $table = filter_input(INPUT_GET, 'table');
-        $result->table = $table;
 
         $value = filter_input(INPUT_GET, 'value') !== null ? filter_input(INPUT_GET, 'value') : "";
         $value_escaped = $conn->real_escape_string($value);
@@ -45,9 +42,6 @@ if ($conn->connect_error) {
             for ($i = 0; $i < mysqli_num_rows($results); $i++) {
                 $result->data[] = mysqli_fetch_object($results);
             }
-            
-            // If there are results..
-            $result->num_res = $results->num_rows;
         }
     } else {
         $result->error = "No table selected";
