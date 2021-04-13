e77a7b4469042a75de58c042eec75d48bcc9a60c
Zowiezo101 <devnull@localhost>
2021-04-13 18:42:40 +0200

    Working on settings tab

--- web/settings.php
+++ web/settings.php
@@ -4,3 +4,17 @@
     $id = basename(filter_input(INPUT_SERVER, 'PHP_SELF'), '.php');
     require 'page/template.php';
 ?>
+
+<script>
+    function onLoadSettings() {
+        $("#content").append(
+            $("<div>").addClass("container-fluid").append(
+                $("<div>").addClass("row")
+                    // The column with the tabs
+                    .append(getTabsMenu())
+                    // The column with the selected tabs
+                    .append(getTabsContent())
+            )
+        );
+    }
+</script>
---
+++ web/src/settings.js
@@ -0,0 +1,104 @@
+
+function getTabsMenu() {
+    var menu = $("<div>").addClass("col-3").append(`
+        <ul class="nav nav-pills flex-column">
+            <li class="nav-item"> <a href="" class="active nav-link" data-toggle="pill" data-target="#tabadd"> ADD BLOG <i class="fa fa-plus text-muted fa-lg"></i></a> </li>
+            <li class="nav-item"> <a href="" class="nav-link" data-toggle="pill" data-target="#tabedit"> EDIT BLOG <i class="fa fa-edit text-muted fa-lg"></i></a> </li>
+            <li class="nav-item"> <a href="" class="nav-link" data-toggle="pill" data-target="#tabdelete"> DELETE BLOG <i class="fa fa-trash text-muted fa-lg"></i></a> </li>
+            <li class="nav-item"> <a href="" class="nav-link" data-toggle="pill" data-target="#tabprofile"> PROFILE <i class="fa fa-cog text-muted fa-lg"></i></a> </li>
+        </ul>
+    `);
+    
+    return menu;
+}
+
+function getTabsContent() {
+    var content = $("<div>").addClass("col-9").append(
+        $("<div>").addClass("tab-content")
+            // Tab for adding blogs
+            .append(`
+                    <div class="tab-pane fade show active" id="tabadd" role="tabpanel">
+                      <form class="">
+                        <h2>ADDING A BLOG</h2>
+                        <!-- Title for the blog -->
+                        <div class="form-group"> 
+                            <label>Title</label> 
+                            <input id="add_blog_title" type="text" class="form-control w-75" placeholder="Enter a title" required> 
+                        </div>
+                        <!-- Text for the blog -->
+                        <div class="form-group"> 
+                            <label>Text</label> 
+                            <textarea id="add_blog_text" class="form-control w-75" placeholder="Blog contents" rows="5" required></textarea> 
+                        </div>
+                        <button type="submit" class="btn btn-primary" onclick="addBlog()">Add Blog</button>
+                      </form>
+                    </div>`)
+            // Tab for editing blogs
+            .append(`
+                    <div class="tab-pane fade" id="tabedit" role="tabpanel">
+                      <form class="">
+                        <p class="lead">Use this page to edit a blog</p>
+                        <div class="form-group">
+                          <label for="exampleFormControlSelect1">Example select</label>
+                          <select class="form-control" id="exampleFormControlSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                          </select>
+                        </div>
+                        <div class="form-group"> <label>Email address</label> <input type="text" class="form-control" placeholder="Enter email"> </div>
+                        <div class="form-group"> <label>Password</label> <textarea class="form-control" placeholder="Password" rows="5"></textarea> </div>
+                        <div class="form-group"> <label>Email address</label> <input type="text" class="form-control" placeholder="Enter email"> </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                    </div>`)
+            // Tab for deleting blogs
+            .append(`
+                    <div class="tab-pane fade" id="tabdelete" role="tabpanel">
+                      <form class="">
+                        <p class="lead">In my soul and absorb its power, like the form of a beloved mistress, then I often think with longing. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <div class="form-group">
+                          <label for="exampleFormControlSelect1">Example select</label>
+                          <select class="form-control" id="exampleFormControlSelect1">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                          </select>
+                        </div>
+                        <div class="form-group"> <label>Email address</label> <input type="text" class="form-control" placeholder="Enter email"> </div>
+                        <div class="form-group"> <label>Password</label> <textarea class="form-control" placeholder="Password" rows="5"></textarea> </div>
+                        <div class="form-group"> <label>Email address</label> <input type="text" class="form-control" placeholder="Enter email"> </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                    </div>`)
+            // Tab for profile settings
+            .append(`
+                    <div class="tab-pane fade" id="tabprofile" role="tabpanel">
+                      <p class="">Which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite. When I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms.</p>
+                    </div>`)
+    );
+    
+    return content;
+}
+
+function addBlog() {
+    var blog_title = $("#add_blog_title").val();
+    var blog_text = $("#add_blog_text").val();
+    var blog_user = "Zowiezo101";   // TODO
+    var blog_date = "";
+}
+
+function editBlog() {
+    var blog_title = $("#edit_blog_title").val();
+    var blog_text = $("#edit_blog_text").val();
+    var blog_user = "Zowiezo101";   // TODO
+    var blog_date = "";
+}
+
+function deleteBlog() {
+    
+}
