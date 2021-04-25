dc2b1d89f82d744b16b77ee72ac9eee38c5660e6
Zowiezo101 <devnull@localhost>
2021-04-25 20:04:05 +0200

    Using different files for generic stuff

--- web/api/specials.php
+++ web/api/specials.php
@@ -1,23 +1,6 @@
 <?php
+    require 'api.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-$method_type = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
-
-$table = "specials";
-
-switch($method_type) {
-    case 'GET':
-        // The request is using the GET method
-        require 'crud/read.php';
-        break;
-    
-    default:
-        // Null or some other unsupported method
-        echo $method_type.' is not a supported type';
-        break;
-}
+    $table = "specials";
+    executeRequest($table);
+?>
