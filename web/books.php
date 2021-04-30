f3f29f51fc1bea5dd79f9ec44689355340d920ec
Zowiezo101 <devnull@localhost>
2021-04-30 19:17:57 +0200

    Working on books page

--- web/books.php
+++ web/books.php
@@ -4,3 +4,20 @@
     $id = basename(filter_input(INPUT_SERVER, 'PHP_SELF'), '.php');
     require 'page/template.php';
 ?>
+
+<script>
+    // Function to load the content in the content div
+    function onLoadBooks() {
+        $("#content").append(
+            $("<div>").addClass("container-fluid").append(
+                $("<div>").addClass("row")
+                    // The column with the menu
+                    .append(getBooksMenu())
+                    // A random column to keep the space in between
+                    .append(`<div class="col-md-1"></div>`)
+                    // The column with the selected content
+                    .append(getBooksContent())
+            )
+        );
+    }
+</script>
