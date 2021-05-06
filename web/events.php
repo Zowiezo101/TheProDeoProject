e76a82d1ba9edfbdca152334354bab4685e7c634
Zowiezo101 <devnull@localhost>
2021-05-06 12:41:23 +0200

    Finished events, locations and specials pages

--- web/events.php
+++ web/events.php
@@ -4,3 +4,22 @@
     $id = basename(filter_input(INPUT_SERVER, 'PHP_SELF'), '.php');
     require 'page/template.php';
 ?>
+
+<script>
+    // Function to load the content in the content div
+    function onLoadEvents() {
+        $("#content").append(
+            $("<div>").addClass("container-fluid").append(
+                $("<div>").addClass("row")
+                    // The column with the menu
+                    .append(getItemsMenu())
+                    // The column with the selected content 
+                    .append(getContentDiv())
+            )
+        );
+        
+        // Depending on the selected book, 
+        // we need to get information from the database first
+        getItemsContent();
+    }
+</script>
