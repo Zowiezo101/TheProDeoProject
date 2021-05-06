72b35252a20449963587eddcec28b573452a9faa
Zowiezo101 <devnull@localhost>
2021-05-06 16:20:08 +0200

    Added details for books

--- web/src/pages/books.js
+++ web/src/pages/books.js
@@ -1,41 +1,31 @@
 
 
 function getBookContent(books) {
-    // A book has been selected, show it's information
-    var content = $("#item_content").append(`
-        <div class="row">
-            <div class="col-lg-11 px-lg-5 px-md-3 text-center">
-                <h1 class="mb-3">` + books.data[0].name + `</h1>
-                <p class="lead">` + books.data[0].summary + `</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-11 px-lg-5 px-md-3">
-                <div class="table-responsive">
-                    <table class="table table-striped table-borderless">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                            </tr>
-                        </tbody>
-                    </table>
+    if (books && (books.data.length > 0)) {
+    
+        // A book has been selected, show it's information
+        var content = $("#item_content").append(`
+            <div class="row">
+                <div class="col-lg-11 px-lg-5 px-md-3 text-center">
+                    <h1 class="mb-3">` + books.data[0].name + `</h1>
+                    <p class="lead">` + books.data[0].summary + `</p>
                 </div>
             </div>
-        </div>
-    `);
+            <div class="row">
+                <div class="col-lg-11 px-lg-5 px-md-3 text-center">
+                    <p class="lead font-weight-bold mt-4">` + dict["items.details"] + `</p>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-borderless">
+                            <tbody>` +
+                                insertDetail(books.data[0], "num_chapters") + 
+                            `</tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        `);
+        
+    } else {
+        // TODO Foutmelding, niet kunnen vinden?
+    }
 }
