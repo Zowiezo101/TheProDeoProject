221af9926905c217d37756dccd1a57f97f4e8498
Zowiezo101 <devnull@localhost>
2022-08-09 20:56:12 +0200

    Updated popper CDN

--- web/page/import.php
+++ web/page/import.php
@@ -1,7 +1,7 @@
     
         <!-- Some libaries needed for easier programming -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous" style=""></script>
 
         <!-- Extra functionality per page -->
