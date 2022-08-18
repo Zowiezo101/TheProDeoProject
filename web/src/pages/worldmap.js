69c158108545a02e4888d15c5ccf90f87a48a386
Zowiezo101 <devnull@localhost>
2022-08-18 16:44:34 +0200

    Some changed made for zooming When a marker is clicked on the map, just show information and don't pan/zoom When a marker is double clicked on the map, pan and zoom. Don't show information When a marker is clicked in the sidebar, pan, zoom and show information If zoom is changed, check is a marker is in cluster

--- web/src/pages/worldmap.js
+++ web/src/pages/worldmap.js
@@ -42,16 +42,8 @@ function showMap() {
                     
                     // Did we come here via the sidebar?
                     if (marker.button) {
-                        // Move to marker
-                        map.setCenter(marker.getPosition());
-                        
-                        // Ignore the existing zoom
-                        // TODO: Actually check the type that this location
-                        // is and use that to get the best suited zoom, take
-                        // the closest zoom
-                        map.setZoom(minZoomForMarker(marker.id));
-                        map.panBy(0, -Math.round($("#google_maps").height() / 6));
                         marker.button = false;
+                        new google.maps.event.trigger(marker, "dblclick");
                     } else {
                         // Otherwise, stay at the same level as previous
                         // Can't click this location anyway when in cluster
@@ -77,6 +69,24 @@ function showMap() {
                     // Open the info window
                     infoWindow.setContent(setContent(location));
                     infoWindow.open(map, marker);
+                    infoWindow.id = location.id;
+                });
+                
+                marker.addListener("dblclick", () => {     
+                    
+                    // Move to marker
+                    map.setCenter(marker.getPosition());
+
+                    // Ignore the existing zoom
+                    var minZoom = minZoomForMarker(marker.id);
+                    var typeZoom = minZoomForType(marker.id);
+                    
+                    // Actually check the type that this location
+                    // is and use that to get the best suited zoom, take
+                    // the closest zoom. Don't zoom out
+                    var zoom = Math.max(minZoom, typeZoom);
+                    map.setZoom(zoom);
+                    map.panBy(0, -Math.round($("#google_maps").height() / 6));
                 });
                 
                 marker.id = location.id;
@@ -98,6 +108,23 @@ function showMap() {
 
                     // Pan to the item
                     getLinkToMap(id);
+                }
+            });
+            
+            map.addListener("zoom_changed", () => {
+                    
+                // Is there a window open?
+                if (infoWindow !== null) {
+                    // Check the minimum zoom needed to show this marker
+                    // and its infoWindow
+                    var minZoom = minZoomForMarker(infoWindow.id);
+                    var curZoom = map.getZoom();
+                    
+                    if (curZoom < minZoom) {
+                        // This marker is no longer out of a cluster
+                        // The infoWindow will disappear, close it
+                        infoWindow.close();
+                    }
                 }
             });
         }
@@ -190,4 +217,8 @@ function drawInfoWorldMapButton() {
             </div>
           </div>
         </div>`);
+}
+
+function minZoomForType(id) {
+    return 8;
 }
