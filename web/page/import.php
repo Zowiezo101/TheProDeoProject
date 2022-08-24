ce4c9903118184d8478dbc4caf6f3e56d07d60ca
Zowiezo101 <devnull@localhost>
2022-08-24 14:37:52 +0200

    Moved CSS files to the import file as well

--- web/page/import.php
+++ web/page/import.php
@@ -7,38 +7,47 @@
 
         <!-- Extra functionality per page -->
         <script src="/src/pages/<?php echo $id; ?>.js"></script>
-        
 <?php if (in_array($id, ["books", "events", "peoples", "locations", "specials", "familytree", "timeline", "worldmap"])) { ?>
+        
         <!-- For the sidebar used with many pages -->
         <script src="/src/tools/items.js"></script>
 <?php } ?>
         
+        <!-- The style sheets -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+        <link rel="stylesheet" href="/css/theme_<?php echo $theme; ?>.css">
+      
+        <!-- Fav icons -->
+        <link rel="icon" type="image/png" sizes="32x32" href="/../favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/../favicon-16x16.png">
 <?php if (in_array($id, ["search"])) { ?>
+        
         <!-- Bootstrap slider -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/11.0.2/bootstrap-slider.js"></script>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" type="text/css">
         <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
-<?php } ?>
+        <link rel="stylesheet" href="/css/slider_<?php echo $theme; ?>.css">
+<?php } else if (in_array($id, ["familytree", "timeline"])) { ?>
         
-<?php if (in_array($id, ["familytree", "timeline"])) { ?>
-        <!-- -->
+        <!-- Tools for navigating and downloading the map -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/svg.js/3.1.1/svg.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/svg-pan-zoom@3.5.0/dist/svg-pan-zoom.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/svgsaver@0.9.0/browser.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/canvas-toBlob@1.0.0/canvas-toBlob.min.js"></script>
   
-        <!-- The family tree maker -->
+        <!-- The map maker -->
         <script src="/src/map/calc.js"></script>
         <script src="/src/map/draw.js"></script>
         <script src="/src/map/view.js"></script>
 <?php } else if (in_array($id, ["worldmap"])) { ?>
+        
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAyFq1pKyxT7asd87wAgr83_yWIrT-sz7E&v=weekly"></script>
         <script src="https://unpkg.com/@googlemaps/markerclusterer/dist/index.min.js"></script>
-<?php } ?>
-
-<?php if (in_array($id, ["settings"])) { ?>
-        <!-- Main Quill library -->
+<?php } else if (in_array($id, ["settings"])) { ?>
+        
+        <!-- Main Summernote library -->
         <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 <?php } ?>
--- web/page/template.php
+++ web/page/template.php
@@ -51,7 +51,6 @@
     }
 ?>
     
-    
     <head>
         <!-- Name shown on the tab -->
         <title><?php echo $dict["globals.prodeo_database"] ?></title>
@@ -59,24 +58,9 @@
         <!-- Some extra information used for viewing -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <!-- The style sheets -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-        <link rel="stylesheet" href="/css/theme_<?php echo $theme; ?>.css">
-<?php if (in_array($id, ["search"])) { ?>
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" type="text/css">
-        <link rel="stylesheet" href="/css/slider_<?php echo $theme; ?>.css">
-<?php } ?>
-        <!-- Include stylesheet -->
-        <link rel="stylesheet" href="https://cdn.quilljs.com/1.3.6/quill.snow.css">
-      
-        <!-- Fav icons -->
-        <link rel="icon" type="image/png" sizes="32x32" href="/../favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/../favicon-16x16.png">
 
-        <!-- External Javascript files -->
+        <!-- External Javascript and script files -->
 <?php require "page/import.php"; ?>
-        
         
         <script>
 
