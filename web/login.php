6c4f1d0cdfaa1bf03dca2b42b21e3ffb57855040
Zowiezo101 <91847374+Zowiezo101@users.noreply.github.com>
2024-04-10 14:24:16 +0200

    Small PHP updates

--- web/login.php
+++ web/login.php
@@ -20,6 +20,19 @@
 
 <script>
     $(function() {
+        var username = {
+            param: "<?php echo $param_username; ?>",
+            err: "<?php echo $username_err; ?>",
+            attr: "<?php echo (!empty($username_err)) ? 'is-invalid' : ((!empty($username)) ? 'is-valid' : ''); ?>"
+        };
+        
+        var password = {
+            param: "<?php echo $param_password1; ?>",
+            err: "<?php echo $password1_err; ?>",
+            attr: "<?php echo (!empty($password1_err)) ? 'is-invalid' : ((!empty($password1)) ? 'is-valid' : ''); ?>"
+        };
+        
+        var login_err = `<?php echo (!empty($login_err)) ? '<div class="alert alert-danger">' . $dict[$login_err] . '</div>' : ''; ?>`;
     
         $("#content").append(
             $("<div>").addClass("container-fluid").append(
@@ -28,7 +41,7 @@
                     .append(
                         $("<div>").addClass("col-3").append(`
                             <ul class="nav nav-pills flex-column">
-                                <li class="nav-item"> <a href="" class="active nav-link" data-toggle="pill" data-target="#tablogin"> <i class="fa fa-user-circle text-muted fa-lg"></i> <?php echo strtoupper($dict["settings.login"]); ?></a> </li>
+                                <li class="nav-item"> <a href="" class="active nav-link" data-toggle="pill" data-target="#tablogin"> <i class="fa fa-user-circle text-muted fa-lg"></i>` + dict["settings.login"].toUpperCase() + `</a> </li>
                             </ul>
                         `))
                     // The column with the tab contents
@@ -36,24 +49,19 @@
                         $("<div>").addClass("col-9").append(
                             $("<div>").addClass("tab-content").append(`
                                 <div class="tab-pane fade show active col-lg-6 col-10" id="tablogin" role="tabpanel">
-
-                                    <?php 
-                                    if(!empty($login_err)){
-                                        echo '<div class="alert alert-danger">' . $dict[$login_err] . '</div>';
-                                    }        
-                                    ?>
+                                    ` + login_err + `
                                     <form class="text-left" action="login" method="post" name="login">
                                         <div class="form-group">
-                                            <label for="email_username"><?php echo $dict["settings.username"]; ?></label>
-                                            <input type="text" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ((!empty($username)) ? 'is-valid' : ''); ?>" name="username" id="email_username" value="<?php echo $param_username; ?>">
-                                            <span class="invalid-feedback"><?php echo $dict[$username_err]; ?></span>
+                                            <label for="email_username">` + dict["settings.username"] + `</label>
+                                            <input type="text" class="form-control ` + username.attr + `" name="username" id="email_username" value="` + username.param + `">
+                                            <span class="invalid-feedback">` + dict[username.err] + `</span>
                                         </div>
                                         <div class="form-group">
-                                            <label for="password"><?php echo $dict["settings.password"]; ?></label>
-                                            <input type="password" class="form-control <?php echo (!empty($password1_err)) ? 'is-invalid' : ((!empty($password1)) ? 'is-valid' : ''); ?>" name="password" id="password" value="<?php echo $param_password1; ?>">
-                                            <span class="invalid-feedback"><?php echo $dict[$password1_err]; ?></span>
+                                            <label for="password">` + dict["settings.password"] + `</label>
+                                            <input type="password" class="form-control ` + password.attr + `" name="password" id="password" value="` + password.param + `">
+                                            <span class="invalid-feedback">` + dict[password.err] + `</span>
                                         </div>
-                                        <button type="submit" name="login" class="btn btn-primary"><?php echo $dict["settings.login"]; ?></button>
+                                        <button type="submit" name="login" class="btn btn-primary">` + dict["settings.login"] + `</button>
                                     </form>
                                 </div>
                             `)
