444b1c4ee128dbd9b1a3da72bfceb6763a4a63fb
Zowiezo101 <91847374+Zowiezo101@users.noreply.github.com>
2025-01-28 21:37:32 +0100

    Will be adding this to a separate branch

--- web-feature/src/template/scripts.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Check if this file exists
-    $page_script = "src/scripts/{$page_id}.php";
-    if (is_file($page_script)) {
-        // If this file exists, require it
-        require $page_script; 
-    }
